09ca6398a1e63f3619b9f78d98236b6c16645577
Alban Dericbourg <alban@dericbourg.net>
2014-08-28 14:09:17 +0200

    Add formatting option.

--- src/main/java/net/dericbourg/fuzzywallhack/api/GenerationConfiguration.java
+++ src/main/java/net/dericbourg/fuzzywallhack/api/GenerationConfiguration.java
@@ -4,4 +4,14 @@ package net.dericbourg.fuzzywallhack.api;
  * Configure here JSON generation.
  */
 public class GenerationConfiguration {
+
+    private boolean formatted = true;
+
+    public boolean isFormatted() {
+        return formatted;
+    }
+
+    public void setFormatted(boolean formatted) {
+        this.formatted = formatted;
+    }
 }
--- src/main/java/net/dericbourg/fuzzywallhack/data/RootData.java
+++ src/main/java/net/dericbourg/fuzzywallhack/data/RootData.java
@@ -2,6 +2,11 @@ package net.dericbourg.fuzzywallhack.data;
 
 import net.dericbourg.fuzzywallhack.api.GenerationConfiguration;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 public class RootData {
 
     private final GenerationConfiguration configuration;
@@ -18,6 +23,13 @@ public class RootData {
 
     public String generate() {
         String generate = data.generate();
+        if (configuration.isFormatted()) {
+            JsonParser parser = new JsonParser();
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            JsonElement el = parser.parse(generate);
+            generate = gson.toJson(el);
+        }
         return generate;
     }
 }
